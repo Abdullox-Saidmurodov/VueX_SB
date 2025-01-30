c68f8a37a0a056b8f74d11fcada09bf389e58d8c
Abdullox Saidmurodov <a.saidmurodov@mail.ru>
2025-01-30 11:25:59 +0500

    4

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,31 @@
 <template lang="">
     <div>
-        Home page
+        Home page {{counter}}
+        <div class="w-25">
+            <Button @click="increaseHandler">Increase</Button>
+        </div>
+        <div class="w-25">
+            <Button @click="decreaseHandler">Decrease</Button>
+        </div>
     </div>
 </template>
 <script>
 export default {
-    
+    methods: {
+        increaseHandler() {
+            // this.$store.state.count++
+            this.$store.commit('increment')
+        },
+        decreaseHandler() {
+            // this.$store.state.count++
+            this.$store.commit('decrement')
+        },
+    },
+    computed: {
+        counter() {
+            return this.$store.state.count
+        }
+    }
 }
 </script>
 <style lang="">
