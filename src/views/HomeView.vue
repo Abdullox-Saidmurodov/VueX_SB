e3e66c956191e0b8cac1175c41c17bf393233af7
Abdullox Saidmurodov <a.saidmurodov@mail.ru>
2025-01-31 18:59:41 +0500

    17

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,24 @@
 <template lang="">
-    <div>
+    <!-- <div>
         Home page 
-    </div>
+    </div> -->
     <!-- {{data}} -->
+    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+        <ArticleCard v-for="article in data" :article="article" :key="article.id" />
+    </div>
 </template>
 <script>
 import { mapState } from 'vuex'
+import ArticleCard from '@/components/ArticleCard.vue';
 export default {
-    methods: {
+    components: {
+        ArticleCard,
     },
     computed: {
         ...mapState({
-        data: state => state.articles.data,
-        isLoading: state => state.articles.isLoading,
-        error: state => state.articles.error,
+            data: state => state.articles.data,
+            isLoading: state => state.articles.isLoading,
+            error: state => state.articles.error,
         })
     },
     mounted() {
