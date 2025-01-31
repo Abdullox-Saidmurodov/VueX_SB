9a638f9c0fd3b342e25bf5b4d0c67f4ef34f66ad
Abdullox Saidmurodov <a.saidmurodov@mail.ru>
2025-01-31 10:45:43 +0500

    12

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,19 +4,36 @@
         <img :src="logo" alt="logo" class="w-25" style="width: 100px; cursor: pointer;">
       </RouterLink>
 
+      <!-- {{user}} -->
+      <!-- {{isLoggedIn}} -->
+
       <nav class="d-inline-flex mt-2 mt-md-0 ms-md-auto">
-        <RouterLink :to="{name: 'login'}" class="me-3 py-2 link-body-emphasis text-decoration-none">Login</RouterLink>
-        <RouterLink :to="{name: 'register'}" class="me-3 py-2 link-body-emphasis text-decoration-none">Register</RouterLink>
+        <template v-if="isLoggedIn">
+          <RouterLink :to="{name: 'home'}" class="me-3 py-2 link-body-emphasis text-decoration-none">
+            {{currentUser.username}}
+          </RouterLink>
+        </template>
+        <template v-if="!isLoggedIn">
+          <RouterLink :to="{name: 'login'}" class="me-3 py-2 link-body-emphasis text-decoration-none">Login</RouterLink>
+          <RouterLink :to="{name: 'register'}" class="me-3 py-2 link-body-emphasis text-decoration-none">Register</RouterLink>
+        </template>
       </nav>
     </div>
 </template>
 <script>
+import { mapState } from 'vuex';
 import {logo} from '../constants'
 export default {
     data() {
       return {
         logo,
       }
+    },
+    computed: {
+      ...mapState({
+        currentUser: state => state.auth.user,
+        isLoggedIn: state => state.auth.isLoggedIn,
+      }),
     },
 }
 </script>
