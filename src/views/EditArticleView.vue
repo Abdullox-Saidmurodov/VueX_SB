1f697a9657d20d9534a35dee48560aa923bdeca4
Abdullox Saidmurodov <a.saidmurodov@mail.ru>
2025-02-03 16:09:28 +0500

    28

--- src/views/EditArticleView.vue
+++ src/views/EditArticleView.vue
@@ -1,26 +1,23 @@
 <template lang="">
-    <!-- <div>
-        Edit
-    </div> -->
-    <p class="text-center display-2">Edit article</p>
-    <!-- <ArticleForm v-if="article"
-                 :title="article.title"
-                 :description="article.description"
-                 :body="article.body"
-                  /> -->
-    <div v-if="article" class="w-50 mx-auto">
-        <form @submit.prevent>
-            <Input type="text" 
-                   label="Title"
-                   v-model="article.title" />
-            <TextArea type="text" 
-                      label="Description"
-                      v-model="article.description"/>
-            <TextArea type="text" 
-                      label="Body" 
-                      v-model="article.body" />
-            <Button @click="editArticleHandler" :disabled='isLoading'>Edit article</Button>
-        </form>
+    <div v-if="isLoading" class="d-flex justify-content-center">
+        <Loader />
+    </div>
+    <div v-else>
+        <p class="text-center display-2">Edit article</p>
+        <div v-if="article" class="w-50 mx-auto">
+            <form @submit.prevent>
+                <Input type="text" 
+                    label="Title"
+                    v-model="initialValue.title" />
+                <TextArea type="text" 
+                        label="Description"
+                        v-model="initialValue.description"/>
+                <TextArea type="text" 
+                        label="Body" 
+                        v-model="initialValue.body" />
+                <Button @click="editArticleHandler" :disabled='isLoadingC'>Edit article</Button>
+            </form>
+        </div>
     </div>
 </template>
 <script>
@@ -31,18 +28,30 @@ export default {
     methods: {
         editArticleHandler() {
             const article = {
-                title: this.article.title,
-                description: this.article.description,
-                body: this.article.body,
+                title: this.initialValue.title,
+                description: this.initialValue.description,
+                body: this.initialValue.body,
                 tagList: [],
             }
-            console.log(article)
+            // console.log(article)
+            // console.log(this.article)
+            this.$store.dispatch('updateArticle', {article: article, slug: this.$route.params.slug})
+            .then(() => this.$router.push('/'))
         },
     },
     computed: {
         ...mapState({
             article: state => state.articles.articleDetail,
-        })
+            isLoading: state => state.articles.isLoading,
+            isLoadingC: state => state.controlArticle.isLoading,
+        }),
+        initialValue() {
+            return {
+                title: this.article.title,
+                description: this.article.description,
+                body: this.article.body,
+            }
+        },
     },
     mounted() {
         this.$store.dispatch('articleDetail', this.$route.params.slug)
