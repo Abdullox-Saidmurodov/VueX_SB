ba5a1966b3b8e409937380902d0ca6f7d6ffe37a
Abdullox Saidmurodov <a.saidmurodov@mail.ru>
2025-02-03 10:41:16 +0500

    24

--- src/components/CreateArticle.vue
+++ src/components/CreateArticle.vue
@@ -1,5 +1,10 @@
 <template lang="">
     <h1>Create-article</h1>
+    <div class="w-50 mx-auto">
+        <form>
+            <Input />
+        </form>
+    </div>
 </template>
 <script>
 export default {
--- src/views/CreateArticleView.vue
+++ src/views/CreateArticleView.vue
@@ -1,9 +1,34 @@
 <template lang="">
-    <h1>Create-article</h1>
+    <p class="text-center display-2">Create-article</p>
+    <div class="w-50 mx-auto">
+        <form @submit.prevent>
+            <Input type="text" label="Title" v-model="title" />
+            <TextArea type="text" label="Description" v-model="description" />
+            <TextArea type="text" label="Body" v-model="body" />
+            <Button @click="createArticleHandler">Create article</Button>
+        </form>
+    </div>
 </template>
 <script>
 export default {
-    
+    data() {
+        return {
+            title: '',
+            description: '',
+            body: '',
+        }
+    },
+    methods: {
+        createArticleHandler() {
+            const article = {
+                title: this.title,
+                description: this.description,
+                body: this.body,
+                tagList: [],
+            }
+            console.log(article)
+        },
+    }
 }
 </script>
 <style lang="">
